17dee6402abdcab7af8ef5923a62509433e94a73
chz88818 <496510571@qq.com>
2021-09-22 15:47:57 +0800

    logstash日志收集配置

--- src/main/java/chz/cloud/websocket/chat/ChatApplication.java
+++ src/main/java/chz/cloud/websocket/chat/ChatApplication.java
@@ -1,5 +1,7 @@
 package chz.cloud.websocket.chat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatApplication {
 
     public static void main(String[] args) {
+        Logger logger= LoggerFactory.getLogger(ChatApplication.class);
+        logger.error("自己写的ERROR");
+        logger.info("自己写的INFO");
         SpringApplication.run(ChatApplication.class, args);
     }
 
